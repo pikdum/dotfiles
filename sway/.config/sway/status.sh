46232810b01f5e132e8a0c2b6f06c1f6be3274f7
pikdum <pikdum@kuudere.moe>
2024-03-08 00:09:46 -0600

    feat: get weather only once an hour
    should be nicer to their servers

--- sway/.config/sway/status.sh
+++ sway/.config/sway/status.sh
@@ -9,6 +9,17 @@ date_formatted=$(date "+%a %F %I:%M %p")
 linux_version=$(uname -r | cut -d '-' -f1)
 # battery_status=$(cat /sys/class/power_supply/BAT0/status)
 load_average=$(uptime | awk -F'[a-z]: ' '{ print $2 }' | sed 's/,//g')
-weather=$(curl wttr.in/?format="+%t+%c")
+
+# weather, with caching
+# error handling if curl request fails
+weather_file="/tmp/wttr.in-$(date "+%F-%H").txt"
+if [ ! -f "$weather_file" ]; then
+    curl --silent --max-time 5 https://wttr.in/?format="+%t+%c" > "$weather_file"
+    if [ $? -ne 0 ]; then
+        echo "" > "$weather_file"
+    fi
+fi
+
+weather=$(cat "$weather_file")
 
 echo "$weather $uptime_formatted ⏳   $linux_version 🐧   $load_average 💻   $date_formatted"
